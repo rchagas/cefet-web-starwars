63cd6cde2ed01752ad46f0bdcb522fc8ae9f367e
rchagas <rchagas@users.noreply.github.com>
2017-04-25 12:23:44 -0300

    Add files via upload

--- starwars.js
+++ starwars.js
@@ -3,15 +3,15 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
-let movies = document.getElementById('movies');
+//let movies = document.getElementById('movies');
+let movies = $('#movies > ul');
+movies.innerHTML = '\n';
+const romano = ['I','II','III','IV','V','VI','VII'];
 $.ajax({
   url: 'http://swapi.co/api/films',
   success: function(resposta) {
-    console.log(resposta);
-    /*let sw = JSON.parse(resposta);
-    let html;
-    for(){
-        
-    }*/
+    for(let i=0; i<resposta.count ; i++){
+        movies.append('<li>Episode'+romano[i]+'</li>');
+    }
   }
 });
