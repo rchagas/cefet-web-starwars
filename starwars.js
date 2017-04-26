90976a10882fea2bc6a44d0e6e50fed0ed0eb85a
rchagas <raphaelchagas_@hotmail.com>
2017-04-26 03:37:51 -0300

    Finalização

--- starwars.js
+++ starwars.js
@@ -4,16 +4,26 @@
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
 //let movies = document.getElementById('movies');
-let movies = $('#movies > ul');
-movies.innerHTML = '';
-const romano = ['I','II','III','IV','V','VI','VII'];
-$.ajax({
-  url: 'http://swapi.co/api/films',
-  success: function(resposta) {
-    for(let i=0; i<resposta.count ; i++){
-        movies.append('<li>Episode'+romano[i]+'</li>');
+let movies = document.querySelector('#movies ul');
+let url = 'http://swapi.co/api/films'
+const guide = [[4,'I'],[5,'II'],[6,'III'],[1,'IV'],[2,'V'],[3,'VI'],[7,'VII']];
+let aux = '';
+for(let i=0 ; i<guide.length ; i++){
+     aux += '<li data-episode-url='+url+'/'+guide[i][0]+'/>Episode '+guide[i][1]+'</li>';
+}
+movies.innerHTML = aux;
+let episodes = document.querySelectorAll('#movies li');
+for(let episode of episodes ){
+    episode.addEventListener('click', texto);
+}
+function texto(){
+    $.ajax({
+    url: this.getAttribute('data-episode-url'),
+    success: function(resposta) {
+      document.querySelector('.reading-animation').innerHTML =  
+              'Episode '+guide[resposta.episode_id-1][1]+'\n'
+              +resposta.title+'\n\n'+resposta.opening_crawl;
     }
+  });
+}
 
-
-  }
-});
